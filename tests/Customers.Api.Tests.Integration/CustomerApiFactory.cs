34f3248704e7fe1086a11bd8e245c3b2de13908d
baxa68486 <34898824+baxa68486@users.noreply.github.com>
2022-12-29 00:40:55 +0100

    Add project files.

---
+++ tests/Customers.Api.Tests.Integration/CustomerApiFactory.cs
@@ -0,0 +1,70 @@
+﻿using System.Data.Common;
+using Customers.Api.Database;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Npgsql;
+using Respawn;
+using Xunit;
+
+namespace Customers.Api.Tests.Integration;
+
+public class CustomerApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
+{
+    private readonly PostgreSqlTestcontainer _dbContainer =
+        new TestcontainersBuilder<PostgreSqlTestcontainer>()
+            .WithDatabase(new PostgreSqlTestcontainerConfiguration
+            {
+                Database = "mydb",
+                Username = "nick",
+                Password = "chapsas"
+            }).Build();
+
+    private DbConnection _dbConnection = default!;
+    private Respawner _respawner = default!;
+
+    public HttpClient HttpClient { get; private set; } = default!;
+    
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveAll(typeof(IDbConnectionFactory));
+            services.AddSingleton<IDbConnectionFactory>(_ =>
+                new NpgsqlConnectionFactory(_dbContainer.ConnectionString));
+        });
+    }
+
+    public async Task ResetDatabaseAsync()
+    {
+        await _respawner.ResetAsync(_dbConnection);
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+        _dbConnection = new NpgsqlConnection(_dbContainer.ConnectionString);
+        HttpClient = CreateClient();
+        await InitializeRespawner();
+    }
+
+    private async Task InitializeRespawner()
+    {
+        await _dbConnection.OpenAsync();
+        _respawner = await Respawner.CreateAsync(_dbConnection, new RespawnerOptions
+        {
+            DbAdapter = DbAdapter.Postgres,
+            SchemasToInclude = new[] { "public" }
+        });
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _dbContainer.StopAsync();
+    }
+}
