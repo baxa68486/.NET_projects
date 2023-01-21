49379543abbf33d303d6b557debd7fd5a8014ffb
baxa68486 <34898824+baxa68486@users.noreply.github.com>
2023-01-21 11:26:47 +0100

    added project for unit tests

---
+++ Customer.Unit.Tests/CustomerTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Customer.Unit.Tests
+{
+    public class CustomerTests
+    {
+        [Fact]
+        public void TestMethod()
+        {
+            Assert.True(true);
+        }
+    }
+}
